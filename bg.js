edddc71b956fbdf90290aea3307d684d1eaf1101
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2019-11-20 13:14:52 +0100

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,47 @@
+var can = document.getElementById('canvas'); 
+  
+
+var ctx = can.getContext('2d'); 
+  
+
+can.width = 600; 
+can.height = 600; 
+  
+
+var img = new Image(); 
+  
+
+img.src = "spacebg.png"; 
+  
+
+window.onload = function() { 
+    // the initial image height 
+    var imgHeight = 0; 
+  
+    
+    var scrollSpeed = 10; 
+  
+   
+    function loop() 
+    { 
+        // draw image 1 
+        ctx.drawImage(img, 0, imgHeight); 
+  
+        // draw image 2 
+        ctx.drawImage(img, 0, imgHeight - can.height); 
+  
+        
+        imgHeight += scrollSpeed; 
+  
+        
+        if (imgHeight == can.height) 
+            imgHeight = 0; 
+  
+        
+        window.requestAnimationFrame(loop); 
+    } 
+  
+    
+    loop(); 
+  
+} 
